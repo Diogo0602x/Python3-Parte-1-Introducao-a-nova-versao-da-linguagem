ab425e8fb7070464e66cbf12336a853ecc2639a1
Diogo060x <diogoteixeira0602@gmail.com>
2021-05-17 11:24:33 -0300

    Aula 02 - Atividade 03 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,18 @@
-print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+print("Você digitou: ", chute_str)
+
+chute = int(chute_str)
+
+if (numero_secreto == chute):
+  print("Você acertou!")
+else:
+  print("Você errou!")
+
+print("Fim do jogo")
