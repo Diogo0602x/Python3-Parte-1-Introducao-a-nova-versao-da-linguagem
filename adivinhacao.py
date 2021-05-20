ee52ca280df738592d89c954ec6328c9fd40c8be
Diogo060x <diogoteixeira0602@gmail.com>
2021-05-20 10:26:32 -0300

    Aula 04 - Atividade 04 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,7 +7,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while(rodada <= total_de_tentativas):
-  print("Tentativa", rodada, "de", total_de_tentativas)
+  print("Tentativa {} de {}".format(rodada, total_de_tentativas))
   chute_str = input("Digite o seu número: ")
   print("Você digitou", chute_str)
   chute = int(chute_str)
