368a6bf9f7b22f8dc86ecb9d5372d14bc2d059da
Diogo0602x <diogoteixeira0602@gmail.com>
2021-05-25 09:32:46 -0300

    Aula 05 - Atividade 01 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,8 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
   print("Tentativa {} de {}".format(rodada, total_de_tentativas))
   chute_str = input("Digite o seu número: ")
   print("Você digitou", chute_str)
@@ -23,5 +22,5 @@ while(rodada <= total_de_tentativas):
       print("Você errou! O seu chute foi maior do que o número secreto.")
     if(menor):
       print("Você errou! O seu chute foi menor do que o número secreto.")    
-  rodada = rodada +1    
+
 print("Fim do jogo")
