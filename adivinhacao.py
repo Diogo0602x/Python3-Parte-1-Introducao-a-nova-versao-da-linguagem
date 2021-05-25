31b3117a2353befeb7bbba5bad0a05ed4700d5eb
Diogo0602x <diogoteixeira0602@gmail.com>
2021-05-25 09:53:37 -0300

    Aula 05 - Atividade 04 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
   print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-  chute_str = input("Digite o seu número: ")
+  chute_str = input("Digite um numero entre 1 e 100: ")
   print("Você digitou", chute_str)
   chute = int(chute_str)
+
+  if(chute < 1 or chute > 100):
+    print("Você deve digitar um número entre 1 e 100!") 
+    continue
 
   acertou = chute == numero_secreto
   maior = chute > numero_secreto
@@ -17,6 +21,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
   if (acertou):
     print("Você acertou!")
+    break
   else:
     if(maior):
       print("Você errou! O seu chute foi maior do que o número secreto.")
