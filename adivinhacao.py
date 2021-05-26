3d74a608ad73af9e2e92b0caf4764e1026907334
Diogo0602x <diogoteixeira0602@gmail.com>
2021-05-26 11:13:01 -0300

    Aula 07 - Atividade 02 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,6 +6,7 @@ print("*********************************")
 
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
+pontos = 1000
 
 print("Qual nível de dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil ")
@@ -34,12 +35,14 @@ for rodada in range(1, total_de_tentativas + 1):
   menor   = chute < numero_secreto
 
   if(acertou):
-    print("Você acertou!")
+    print("Você acertou e fez {} pontos!". format(pontos))
     break
   else:
     if(maior):
       print("Você errou! O seu chute foi maior do que o número secreto.")
     elif(menor):
       print("Você errou! O seu chute foi menor do que o número secreto.")
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
