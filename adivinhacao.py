455d0b84ad21e005779c4c7a68880485d7808dc1
Diogo0602x <diogoteixeira0602@gmail.com>
2021-05-26 10:54:24 -0300

    Aula 07 - Atividade 01 terminada

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,19 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil ")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+  total_de_tentativas = 20
+elif(nivel == 3):
+  total_de_tentativas = 10
+else:
+  total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
   print("Tentativa {} de {}".format(rodada, total_de_tentativas))
