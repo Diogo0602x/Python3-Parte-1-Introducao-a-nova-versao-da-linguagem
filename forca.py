bdd1375a4b3571d6ff96226f529de153fbebc9ae
Diogo0602x <diogoteixeira0602@gmail.com>
2021-05-31 17:26:38 -0300

    Jogo Finalizado

--- forca.py
+++ forca.py
@@ -1,6 +1,9 @@
 def jogar():
-  print("*********************************")
-  print("***Bem vindo ao jogo da forca!***")
-  print("*********************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-print("Fim do jogo")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
--- jogos.py
+++ jogos.py
@@ -1,17 +1,21 @@
 import forca
 import adivinhacao
 
-print("*********************************")
-print("*******Escolha o seu jogo!*******")
-print("*********************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo? "))
+    jogo = int(input("Qual jogo? "))
 
-if (jogo == 1):
-  print("Jogando forca")
-  forca.jogar()
-elif (jogo == 2):
-  print("Jogando adivinhação")
-  adivinhacao.jogar()
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
